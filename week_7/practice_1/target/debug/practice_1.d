8b3b359b772aba16b40284d6709dde119a300837
Guvnor Ekekenta <guvnor.ekekenta@PAU.edu.ng>
2025-11-12 13:42:21 +0100

    (Invoking a function)

---
+++ week_7/practice_1/target/debug/practice_1.d
@@ -0,0 +1,1 @@
+C:\Users\USER\Documents\g.ekekentaCOS-101\week_7\practice_1\target\debug\practice_1.exe: C:\Users\USER\Documents\g.ekekentaCOS-101\week_7\practice_1\src\main.rs
---
+++ week_7/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\USER\Documents\g.ekekentaCOS-101\week_7\practice_2\target\debug\practice_2.exe: C:\Users\USER\Documents\g.ekekentaCOS-101\week_7\practice_2\src\main.rs

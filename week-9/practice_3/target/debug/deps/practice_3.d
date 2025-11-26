8e9b8c463b6970622ea65f85e012e6680196faa0
Guvnor Ekekenta <guvnor.ekekenta@PAU.edu.ng>
2025-11-26 13:20:40 +0100

    (Deleting a file using rust)

--- week-9/practice_3/target/debug/deps/practice_3.d
+++ week-9/practice_3/target/debug/deps/practice_3.d
@@ -1,5 +1,5 @@
-C:\Users\USER\Documents\Computer\g.ekekentaCOS-101\week-9\practice_3\target\debug\deps\practice_3.d: src\main.rs
+C:\Users\USER\Documents\g.ekekentaCOS-101\week-9\practice_3\target\debug\deps\practice_3.d: src\main.rs
 
-C:\Users\USER\Documents\Computer\g.ekekentaCOS-101\week-9\practice_3\target\debug\deps\practice_3.exe: src\main.rs
+C:\Users\USER\Documents\g.ekekentaCOS-101\week-9\practice_3\target\debug\deps\practice_3.exe: src\main.rs
 
 src\main.rs:

6525509a02e59ec8ea13b9764c0f5bcb5bba1475
Guvnor Ekekenta <guvnor.ekekenta@PAU.edu.ng>
2025-11-26 13:27:48 +0100

    (Writing to a file using rust)

--- week-9/practiice_1/target/debug/practiice_1.d
+++ week-9/practiice_1/target/debug/practiice_1.d
@@ -1,1 +1,1 @@
-C:\Users\USER\Documents\Computer\g.ekekentaCOS-101\week-9\practiice_1\target\debug\practiice_1.exe: C:\Users\USER\Documents\Computer\g.ekekentaCOS-101\week-9\practiice_1\src\main.rs
+C:\Users\USER\Documents\g.ekekentaCOS-101\week-9\practiice_1\target\debug\practiice_1.exe: C:\Users\USER\Documents\g.ekekentaCOS-101\week-9\practiice_1\src\main.rs
